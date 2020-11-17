3d99ceecb0a8ade6a9ff4ba835df2c1d257b8594
Husan <abdigafurovhusan@gmail.com>
2020-11-17 13:43:40 +0500

    qwerty

--- js/script.js
+++ js/script.js
@@ -26,8 +26,9 @@ burger_icon.addEventListener('click', function () {
 var video = document.querySelector('.video');
 var play_icon = document.querySelector('.play_icon');
 var video_text = document.querySelector('.video_text');
+var timer = document.querySelector('.video_num');
 video_text.addEventListener('click', function () {
-    if (play_icon.classList.contains('play')) {
+    if (video.paused) {
         video.play();
         play_icon.classList.add('pause');
         play_icon.classList.remove('play');
@@ -40,10 +41,11 @@ video_text.addEventListener('click', function () {
         play_icon.setAttribute('src', 'img/pause_icon.png');
         video_text.style.opacity = 1;
     }
+    timer.innerText = Math.round(video.currentTime / video.duration * 100) + "%";
 })
 
 video.addEventListener('click', function () {
-    if (play_icon.classList.contains('play')) {
+    if (video.paused) {
         video.play();
         play_icon.classList.add('pause');
         play_icon.classList.remove('play');
@@ -55,9 +57,21 @@ video.addEventListener('click', function () {
         video.pause();
         play_icon.setAttribute('src', 'img/play_icon.png');
         video_text.style.opacity = 1;
+        
     }
+    timer.innerText = Math.round(video.currentTime / video.duration * 100) + "%";
 })
+// function video_play() {
+//     if (video.paused) {
+//         video_text.style.opacity = 0;
+//     }
+//     else{
+//         video_text.style.opacity = 1;
+//     }
 
+//     video_play();
+//   }
+//   video_play();
 /*================================= video =======================================*/
 
 
